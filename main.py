0ca6b02ef558a751d2ff14dcd688d6170b75b91f
bakerr93 <64405654+bakerr93@users.noreply.github.com>
2022-11-01 09:13:52 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,17 @@
-import random
+s = input("what is your string?\n>")
+s = s + ' '
+current_string = ""
+longest_string = ""
 
-def Get_ToF_statements():
-  statements = []
-  statements.append(["The tallest mountain is Everest", "true", "True"])
-  statements.append(["The largest forest is the Amazon", "true", "True"])
-  statements.append(["USA is the most populous country", "false", "False"])
-  statements.append(["Brown eyes are a dominant gene", "true", "True"])
-  statements.append(["Cheese is made of trees", "false", "False"])
+for x in range(len(s) - 1):
+  t = 0
+  while s[x + t] <= s[x + 1 + t]:
 
-  return statements
+    if x + 1 + t >= len(s) - 1:
+      break
+    t = t + 1
+    current_string = (s[x:x + t + 1])
 
-
-print("type 'stop' to end the game")
-
-def Ask_questions():
-  stop = False
-  ToF_statements = Get_ToF_statements()
-  random.shuffle(ToF_statements)
-  score = 0
-  while stop is False:
-    for s in ToF_statements:
-      print(s[0] + ": True or false?")
-      user_answer = input(">")
-      if user_answer == s[1] or user_answer == s[2]:
-        print("Congrats!")
-        score = score + 1
-      elif user_answer == "stop":
-        stop = True
-        score = str(score)
-        print("your score is " + score)
-      else:
-        print("better luck next time")
-
-Ask_questions()
+    if len(current_string) > len(longest_string):
+      longest_string = current_string
+print("Your longest string is:", longest_string)
